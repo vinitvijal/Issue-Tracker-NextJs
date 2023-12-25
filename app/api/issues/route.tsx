2255fbfb4a7251720f7bb00275e924f573a08a90
Vinu's MacBook Pro <Vinit Vijal>
2023-12-25 15:24:32 +0530

    minor changes

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod';
+
+const schema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)
+})
+
+export async function POST(request: NextRequest){
+    const body = await request.json();
+    const validation = schema.safeParse(body);
+    if (!validation.success){
+        return NextResponse.json(validation.error.errors, {status: 400})
+    }
+
+}
