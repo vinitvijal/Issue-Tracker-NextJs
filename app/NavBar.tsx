d66999e7ef593eb63920edb6be62c175f12eddc9
Vinu's MacBook Pro <Vinit Vijal>
2023-12-25 13:44:58 +0530

    Navbar Done

---
+++ app/NavBar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+import {AiFillBug} from 'react-icons/ai'
+
+const NavBar = () => {
+  const links = [
+    { label: 'Dashboard', href: '/'},
+    { label: 'Issues', href: '/issues'},
+    { label: 'About', href: '/about'},
+  ]
+
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center '>
+        <Link href='/'><AiFillBug /></Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => (
+                <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>
+            ))}
+            {/* <li><Link className='text-zinc-500 hover:text-zinc-800' href='/'>Dashboard</Link></li>
+            <li><Link href='/issues'>Issues</Link></li> */}
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
