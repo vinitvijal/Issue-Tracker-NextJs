ce36c50eff7b0062b732a9e49da27c4bc06f07c5
Vinu's MacBook Pro <Vinit Vijal>
2023-12-26 20:35:31 +0530

    Created API for DB

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
+import prisma from "@/prisma/client";
 
 const schema = z.object({
     title: z.string().min(1).max(255),
@@ -12,5 +13,11 @@ export async function POST(request: NextRequest){
     if (!validation.success){
         return NextResponse.json(validation.error.errors, {status: 400})
     }
+
+    const newIssue = await prisma.issue.create({
+        data : {title: body.title, description: body.description}
+    });
+
+    return NextResponse.json(newIssue, {status: 201})
 
 }
