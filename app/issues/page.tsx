a56db3e82c8c6edfcd3bea89b93994a4c6def7fc
Vinu's MacBook Pro <Vinit Vijal>
2023-12-26 20:39:48 +0530

    Setting up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@radix-ui/themes'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div><Button>New Issue</Button></div>
   )
 }
