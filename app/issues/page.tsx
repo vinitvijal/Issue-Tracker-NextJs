d4807df680280de7b0698af294b3908a5ead7f0b
Vinu's MacBook Pro <Vinit Vijal>
2023-12-27 19:02:27 +0530

    Created New Issues Pages

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   return (
-    <div><Button>New Issue</Button></div>
+    <div><Button><Link href='/issues/new'>New Issue</Link></Button></div>
   )
 }
