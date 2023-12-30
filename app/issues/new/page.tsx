6b4b65ba7b51cba7d3698c31f7e2244e98a9faf8
Vinu's MacBook Pro <Vinit Vijal>
2023-12-30 15:37:08 +0530

    Client Side Validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,22 @@
 'use client'
 import dynamic from "next/dynamic";
-import { Button, Callout, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { schema } from "@/app/validationSchema";
+import { z } from "zod";
 
-
-interface IssueForm{
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof schema>
 
 export default function NewIssuePage() {
   const router = useRouter();
-  const {register, control, handleSubmit} = useForm<IssueForm>();
+  const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
+    resolver: zodResolver(schema)
+  });
   const SimpleMDE = dynamic(
     () => import("react-simplemde-editor"),
     { ssr: false }
@@ -41,12 +42,13 @@ export default function NewIssuePage() {
         <TextField.Root>
             <TextField.Input {...register('title')} placeholder='Title'/>
         </TextField.Root>
+        {errors.title && <Text as="p" color='red'>{errors.title.message}</Text>}
         <Controller
           name='description'
           control={control}
           render={({ field })=> <SimpleMDE {...field}/>}
         />
-        
+        {errors.description && <Text color='red' as="p">{errors.description.message}</Text>}
         <Button>Submit New Issue</Button>
     </form>
         
